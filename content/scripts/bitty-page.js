59d6b18646d2aa1eee5b26d84f5c31d062805ec3
Alan Smith <commit@alanwsmith.com>
2025-10-28 19:03:37 -0400

    starting to get conneciton working

--- content/scripts/bitty-page.js
+++ content/scripts/bitty-page.js
@@ -1,5 +1,21 @@
 export default class {
+  #cw = null;
+
   bittyInit() {
     this.api.fn.setProp("--load-hider", "1");
+  }
+
+  openWindow(event, el) {
+    const params = `scrollbars=no,resizable=yes,status=no,location=no,toolbar=no,menubar=no,width=800,top=20,left=20`
+      this.#cw = window.open(
+          "/speaker.html",
+          "speaker_notes",
+          params
+        );
+    console.log(event);
+  }
+
+  sendMessage(_event, _el) {
+    this.#cw.postMessage("here1");
   }
 };
